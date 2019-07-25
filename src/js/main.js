f03a9388bf05dec831324fb6f1a26b595a0f1772
Mańko <p27746@tvp.pl>
2019-07-25 23:48:19 +0200

    feat: set edited text stored in session storage

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,12 @@
 const editedText = document.querySelector('.editor__text--js');
+
+if(sessionStorage.getItem('sessionText')){
+  editedText.value = sessionStorage.getItem('sessionText');
+  console.log(editedText.value);
+}else{
+  console.log("dupa");
+}
+
 editedText.addEventListener('keyup', (e)=>{
   sessionStorage.setItem('sessionText', e.target.value);
 })
-
-
-
